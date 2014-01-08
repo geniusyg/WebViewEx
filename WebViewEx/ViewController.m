95d68f9fcc6dbe4417dd9d547b3fc72d815f65f0
Chae Yungi <l8673901@gmail.com>
2014-01-09 02:38:15 +0900

    WebViewEx01
    http:// 없어도 됨

--- WebViewEx/ViewController.m
+++ WebViewEx/ViewController.m
@@ -8,16 +8,51 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate, UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
 
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+	if(UIWebViewNavigationTypeLinkClicked == navigationType) {
+		NSLog(@"링크 클릭 금지!");
+		return NO;
+	}
+	return YES;
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+	NSString *msg = [error localizedDescription];
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"에러" message:msg delegate:nil cancelButtonTitle:@"확인" otherButtonTitles: nil];
+	[alert show];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	[self go:nil];
+	return YES;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)go:(id)sender {
+	NSString *address = self.textField.text;
+	if([address length] > 3) {
+		if(![address hasPrefix:@"http://"]) {
+			address = [NSString stringWithFormat:@"http://%@",self.textField.text];
+		}
+		
+		NSURL *url = [NSURL URLWithString:address];
+		NSURLRequest *request = [NSURLRequest requestWithURL:url];
+		[self.webView loadRequest:request];
+		[self.textField resignFirstResponder];
+		
+	}
 }
 
 - (void)didReceiveMemoryWarning
